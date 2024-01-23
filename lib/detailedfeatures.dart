c4be617b26d87a69e296103ab03c73aad27b63d4
Ashraful Haque <ashrafmoin0@gmail.com>
2024-01-23 17:00:10 +0600

    detailedfeatures.dart Update .✔

--- lib/detailedfeatures.dart
+++ lib/detailedfeatures.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Detailedfeatures extends StatefulWidget {
   const Detailedfeatures({Key? key}) : super(key: key);
@@ -36,7 +37,7 @@ class _DetailedfeaturesState extends State<Detailedfeatures> {
                 children: [
                   Container(
                     padding: EdgeInsets.all(8),
-                    height: 180,
+                    height: 150,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(18),
                       color: Color.fromARGB(255, 248, 56, 88),
@@ -48,7 +49,7 @@ class _DetailedfeaturesState extends State<Detailedfeatures> {
                         Container(
                           width: 60,
                           height: 60,
-                          padding: EdgeInsets.only(right: 8),
+                          padding: EdgeInsets.only(left: 8),
                           decoration: BoxDecoration(
                             shape: BoxShape.circle,
                           ),
@@ -107,6 +108,112 @@ class _DetailedfeaturesState extends State<Detailedfeatures> {
                     ),
                   ),
                 ],
+              ),
+            ),
+            Wrap(
+              alignment: WrapAlignment.start,
+              spacing: 10,
+              runSpacing: 10,
+              children: [
+                buildCard(
+                  icon: FontAwesomeIcons.building,
+                  color: Colors.purple,
+                  text: 'Add Order',
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => DistributerList()),
+                    // );
+                  },
+                ),
+                buildCard(
+                  icon: FontAwesomeIcons.shop,
+                  color: const Color(0xFFD9EDBF),
+                  text: 'Order List',
+                  onTap: () {
+                    // Add navigation logic here
+                  },
+                ),
+                buildCard(
+                  icon: FontAwesomeIcons.bullseye,
+                  color: const Color(0xFF6B240C),
+                  text: 'Update Location',
+                  onTap: () {
+                    // Add navigation logic here
+                  },
+                ),
+                buildCard(
+                  icon: FontAwesomeIcons.info,
+                  color: const Color(0xFFFF4A4F),
+                  text: 'Payment List',
+                  onTap: () {
+                    // Add navigation logic here
+                  },
+                ),
+                buildCard(
+                  icon: FontAwesomeIcons.mapLocation,
+                  color: Colors.blueAccent,
+                  text: 'Stock List',
+                  onTap: () {
+                    // Add navigation logic here
+                  },
+                ),
+                buildCard(
+                  icon: FontAwesomeIcons.moneyBillWave,
+                  color: const Color(0xFF365486),
+                  text: 'Visit List',
+                  onTap: () {
+                    // Add navigation logic here
+                  },
+                ),
+                // Add more cards as needed
+                // ...
+              ],
+            ),
+          ],
+
+        ),
+
+      ),
+    );
+  }
+  Widget buildCard({
+    required IconData icon,
+    required Color color,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 3 - 10 * 2,
+        height: 120,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 6,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FaIcon(
+              icon,
+              size: 60,
+              color: color,
+            ),
+            const SizedBox(height: 5),
+            Text(
+              text,
+              style: const TextStyle(
+                fontFamily: 'monospace',
+                fontSize: 10,
+                color: Colors.black,
               ),
             ),
           ],
