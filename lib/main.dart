0fd76aae7fe6e6bbd4e70476d928cdd3cd3b21b0
Ashraful Haque <ashrafmoin0@gmail.com>
2024-01-25 13:04:37 +0600

    ScaffoldBackgroundColor change.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 // import 'package:employe_portal/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:robisalesautomation/utility/mycolors.dart';
 import 'package:robisalesautomation/view/homepage.dart';
 import 'package:robisalesautomation/view/orderdelivery.dart';
 
 
 void main() {
   runApp(MaterialApp(
+    theme: new ThemeData(scaffoldBackgroundColor: Appcolors.lightwhite),
     debugShowCheckedModeBanner: false,
     initialRoute: 'homepage',
     routes: {'homepage': (context) => Homepage()},
